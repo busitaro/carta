3835c618ed03b8ca0aefbdd8335eef6bada1a5f1
busitaro <busitaro10@gmail.com>
2024-02-02 12:50:08 +0900

    :zap: かるたカードを並べて表示するよう対応

--- src/front/carta/src/components/organisms/CartaTable.vue
+++ src/front/carta/src/components/organisms/CartaTable.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="block w-11/12 h-screen mx-auto bg-blue-100">
-    <CartaCard char="ア" />
+  <div class="block w-11/12 h-screen mx-auto bg-blue-100 p-10 flex flex-row">
+    <CartaCard
+      v-for="(char, index) in charactors"
+      :key="index"
+      :char="char"
+      class="m-2"
+    />
     <slot></slot>
   </div>
 </template>
@@ -11,6 +16,19 @@ import CartaCard from "~/components/organisms/CartaCard.vue";
 export default {
   components: {
     CartaCard,
+  },
+  data() {
+    return {
+      charactors: [],
+    };
+  },
+  mounted() {
+    this.charactors = this.getAllCharactors();
+  },
+  methods: {
+    getAllCharactors() {
+      return ["ア", "イ", "ウ"];
+    },
   },
 };
 </script>
